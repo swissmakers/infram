d1b8169c94d036053ce11625d50225a258161daf
Michael Reber <michael.reber@swissmakers.ch>
2026-03-09 13:51:39 +0100

    Cleanup languages and fix malformed json-files

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -4,14 +4,10 @@ import HttpApi from 'i18next-http-backend';
 
 export const languages = [
     {name: 'English', code: 'en'},
-    {name: 'Čeština', code: 'cs'},
     {name: 'Deutsch', code: 'de'},
     {name: 'Español', code: 'es'},
     {name: 'Français', code: 'fr'},
     {name: 'Italiano', code: 'it'},
-    {name: 'Русский', code: 'ru'},
-    {name: 'Português-Brasil', code: 'pt-BR'},
-    {name: '中文', code: 'zh'},
 ]
 
 i18n.use(initReactI18next).use(HttpApi).init({
